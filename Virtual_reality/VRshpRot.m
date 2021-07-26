2a0092b13349c86e2249648ab927614e1edbaa39
necbal <lt6ga@protonmail.com>
2021-07-27 03:09:51 +1200

    add img

--- Virtual_reality/VRshpRot.m
+++ Virtual_reality/VRshpRot.m
@@ -1,14 +1,15 @@
 x=2;
 y = 0;
 z = 0;
-dir = [1 0 0]';
+dir_0 = 1/sqrt(3)
+dir = [dir_0 dir_0 dir_0]';
 
 
 
 c=1/2
 [SS1,SS2,SS3] =  sphere(20);
 S1 = SS1.*c; S2 = SS2.*c; S3 = SS3.*c;
-%surf(5+S1.*(c),5+S2.*(c),5+S3.*(c));
+surf(5+S1.*(c),5+S2.*(c),5+S3.*(c));
 
 %axis([-1 5 -1 5 -.5 5.5])
 axis equal
@@ -30,17 +31,17 @@ for n =1:50
         data=imread('neilba.jpg');
         imshow(data);
     else
-        prompt = 'w,s,a,d: ';
-        inp = input(prompt)
-        if inp == 1
+        prompt = 'Press a,d,u,j and then enter to rotate left, right, up, down \n or press w and then enter to move in the current direction pre ';
+        inp = input(prompt,'s')
+        if inp == 'u'
            dir = rotx(30)*dir; 
-        elseif inp == 2
+        elseif inp == 'j'
             dir = rotx(-30)*dir; 
-        elseif inp == 3
+        elseif inp == 'a'
            dir = rotz(30)*dir; 
-        elseif inp == 4
+        elseif inp == 'd'
             dir = rotz(-30)*dir; 
-        elseif inp == 0
+        elseif inp == 'w'
            x =  x + dir(1) ;
            y =  y + dir(2) ;
            z =  z + dir(3) ; 
@@ -52,3 +53,4 @@ end
 %msgbox('PLANET NOT REACHED, BUT STILL NICE LINEAR ALGEBRA')
        
    
+
--- Virtual_reality/VRsph.m
+++ Virtual_reality/VRsph.m
@@ -1,10 +1,12 @@
+%start
 x=0;
 y = 0;
 z = 0;
+%directions
 fw = [1 0 0];
 bw = [-1 0 0];
-lw = [0 -1 0];
-rw = [0 1 0];
+lw = [0 1 0];
+rw = [0 -1 0];
 uw = [0 0 1];
 dw = [0 0 -1];
 
@@ -13,53 +15,40 @@ c=1/2
 [SS1,SS2,SS3] =  sphere(20);
 S1 = SS1.*c; S2 = SS2.*c; S3 = SS3.*c;
 surf(5+S1.*(c),5+S2.*(c),5+S3.*(c));
-% [verts, faces, cindex] = teapotGeometry;
-% figure
-% p = patch('Faces',faces,'Vertices',verts,'FaceVertexCData',cindex,'FaceColor','interp')
-
-%[U,V,W] = surfnorm(X,Y,Z);
-%quiver3(X,Y,Z,U,V,W,0)
 axis equal
 
-% 
-
-
-
 for n =1:20
-        %figure
     hold on
     title('REACH THE SMALL YELLOW PLANET')
     surf(x+S1,y+S2,z+S3)
-    %surf(S1,S2,S3)
     hold off
     if [x y z] == [5 5 5]
-        %msgbox('WE REACHED THE PLANET, THANKS TO LINEAR ALGEBRA')
-        data=imread('neilba.jpg');
+        data=imread('images/neilba.jpg');
         imshow(data);
     else
-        prompt = 'w,s,a,d: ';
-        inp = input(prompt)
-        if inp == 1
+        prompt = 'Enter either w,s,a,d,u,j and then enter to either move \n forward, back, left, right, up, down (facing in the x direction.)';
+        inp = input(prompt,'s')
+        if inp == 'w'
            x = x+ fw(1);
            y = y+ fw(2);
-           z = z+  fw(3); 
-        elseif inp == 2
-           x = x+ bw(1);
-           y =  y + bw(2) ;
-           z =  z + bw(3) ; 
-        elseif inp == 4
-           x =  x + lw(1) ;
-           y =  y +lw(2) ;
+           z = z+ fw(3); 
+        elseif inp == 's'
+           x = x + bw(1);
+           y = y + bw(2);
+           z = z + bw(3); 
+        elseif inp == 'a'
+           x =  x + lw(1);
+           y =  y + lw(2);
            z =  z + lw(3) ; 
-        elseif inp == 3
+        elseif inp == 'd'
            x =  x + rw(1) ;
            y =  y + rw(2) ;
            z =  z + rw(3) ; 
-        elseif inp == 5
+        elseif inp == 'u'
            x =  x + uw(1) ;
            y =  y +uw(2) ;
            z =  z + uw(3) ; 
-        elseif inp == 6
+        elseif inp == 'j'
            x =  x + dw(1) ;
            y =  y + dw(2) ;
            z =  z + dw(3) ;
@@ -68,11 +57,3 @@ for n =1:20
    
     
 end
-%msgbox('PLANET NOT REACHED, BUT STILL NICE LINEAR ALGEBRA')
-       
-    
-
-  % 
-% [X,Y] = meshgrid(-10:0.25:20,-10:0.2:10);
-% Z = sqrt(-X.^2 -Y.^2+1);
-% surf(X,Y,Z)
